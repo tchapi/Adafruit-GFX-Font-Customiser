a7de067ebbaa4144d5cfa99e7dee851af15c7a25
Billy Donahue <billy.donahue@gmail.com>
2020-06-02 22:15:49 -0400

    mark_points_fix_bounds

---
+++ test/BangOblique24pt7b.h
@@ -0,0 +1,56 @@
+// Processed by `./gfxfont_annotate.py`
+
+const uint8_t BangOblique24pt7bBitmaps[] PROGMEM = {
+
+0x01,0xE0,0x3C,0x0F,0x81,0xE0,0x3C,0x07,0x80,0xF0,0x3C,0x07,0x80,0xF0,0x1E,0x03,0xC0,0xF0,0x1E,0x03,0xC0,0x78,0x0F,0x03,0xC0,0x78,0x0F,0x01,0xE0,0x38,0x07,0x00,0xE0,0x18,0x03,0x00,0x00,0x00,0x00,0x00,0xF0,0x1E,0x07,0x80,0xF0,0x1E,0x00,
+
+// { 0x21 '!' (11 x 34) @(6,-33) +13 }
+// [             ****]
+// [             ****]
+// [            *****]
+// [            **** ]
+// [            **** ]
+// [            **** ]
+// [            **** ]
+// [           ****  ]
+// [           ****  ]
+// [           ****  ]
+// [           ****  ]
+// [           ****  ]
+// [          ****   ]
+// [          ****   ]
+// [          ****   ]
+// [          ****   ]
+// [          ****   ]
+// [         ****    ]
+// [         ****    ]
+// [         ****    ]
+// [         ****    ]
+// [         ***     ]
+// [         ***     ]
+// [         ***     ]
+// [         **      ]
+// [         **      ]
+// [                 ]
+// [                 ]
+// [                 ]
+// [       ****      ]
+// [       ****      ]
+// [      ****       ]
+// [      ****       ]
+// [0     ****   >   ]
+};
+
+const GFXglyph BangOblique24pt7bGlyphs[] PROGMEM = {
+    {0, 11, 34, 13, 6, -33},
+};
+
+const GFXfont FreeSansOblique24pt7b PROGMEM = {
+    (uint8_t *)BangOblique24pt7bBitmaps,
+    (GFXglyph *)BangOblique24pt7bGlyphs,
+    0x21, 0x21, 56
+};
+
+// Approx. 9484 bytes
+
+
